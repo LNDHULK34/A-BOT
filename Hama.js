a7e1e783fcaab50a996745f886a08738123dbbe3
LNDHULK34 <67345382+LNDHULK34@users.noreply.github.com>
2021-01-22 14:42:41 +0300

    Update Hama.js

--- Hama.js
+++ Hama.js
@@ -9,7 +9,7 @@ const { readdirSync } = require("fs");
 const { join } = require("path");
 const client = new Client({ disableMentions: "everyone" });
 const prefix = "."
-client.login("NzM0MTEzMjExNDYwMDkxOTE0.XxM-NA.x6WnVjE6qkH3dbnV1-rTMZDorPQ")
+client.login("Nzc0Njk5MzgwNzA3NDI2MzA0.X6blBQ.R9ghZ52Kb0CDkiHbeMomu8sodiQ")
 client.commands = new Collection();
 client.prefix = prefix;
 client.queue = new Map();
